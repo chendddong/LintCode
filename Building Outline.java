47020a27e797c6e9b348fcadfec21d34a47ba561
Peter <dchen@albany.edu>
2017-05-05 22:47:21 -0400

    Did not even give it a try tho

--- Building Outline.java
+++ Building Outline.java
@@ -1,3 +1,35 @@
+// Given N buildings in a x-axis，each building is a rectangle and can be
+// represented by a triple (start, end, height)，where start is the start
+// position   on x-axis, end is the end position on x-axis and height is the
+// height   of the building. Buildings may overlap if you see them from far
+// away，  find the outline of them。
+
+// An outline can be represented by a triple, (start, end, height), where start 
+// is the start position on x-axis of the outline, end is the end position on  
+// x-axis and height is the height of the outline.
+
+//  Notice
+
+// Please merge the adjacent outlines if they have the same height and make sure
+// different outlines cant overlap on x-axis.
+
+// Have you met this question in a real interview? Yes
+// Example
+// Given 3 buildings：
+
+// [
+//   [1, 3, 3],
+//   [2, 4, 4],
+//   [5, 6, 1]
+// ]
+// The outlines are：
+
+// [
+//   [1, 2, 3],
+//   [2, 4, 4],
+//   [5, 6, 1]
+// ]
+
 import java.util.*;
 
 public class Solution {
@@ -267,3 +299,7 @@ public class Solution {
     }
     return output(res);
   }
+}
+/*
+Did not give this a try 
+*/
