873c88ca625c92a3c97fdf6fd18d5020679cbde3
Peter <dchen@albany.edu>
2017-05-07 18:14:49 -0400

    Nested List Weight Sum Great example both using BFS and DFS

--- Nested List Weight Sum.java
+++ Nested List Weight Sum.java
@@ -1,27 +1,12 @@
-public class Solution {
-    
-    public int depthSum(List<NestedInteger> nestedList) {
-        return helper(nestedList, 1);
-    }
- 
-    public int helper(List<NestedInteger> nestedList, int depth){
-        if (nestedList == null || nestedList.size() == 0)
-            return 0;
+// Nested List Weight Sum Given a nested list of integers, return the sum of all integers in the list weighted by their depth. Each element is either an integer, or a list -- whose elements may also be integers or other lists.
 
-        int sum = 0;
-        for(NestedInteger ele : nestedList) {
-            if (ele.isInteger()) {
-                sum += ele.getInteger() * depth;
-            } else {
-                sum += helper(ele.getList(), depth + 1);
-            }
-        }
- 
-        return sum;
-    }
-}
 
-// 非递归
+// Example
+// Given the list [[1,1],2,[1,1]], return 10. (four 1's at depth 2, one 2 at depth 1, 4 * 1 * 2 + 1 * 2 * 1 = 10)
+// Given the list [1,[4,[6]]], return 27. (one 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 42 + 63 = 27)
+
+// Version Recursive
+
 /**
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
@@ -43,11 +28,44 @@ public class Solution {
  * }
  */
 public class Solution {
+
     public int depthSum(List<NestedInteger> nestedList) {
-        // Write your code here
+        return helper(nestedList, 1);
+    }
+
+    public int helper(List<NestedInteger> nestedList, int depth) {
         if (nestedList == null || nestedList.size() == 0) {
             return 0;
         }
+
+        int sum = 0;
+        for (NestedInteger ele : nestedList) {
+            if (ele.isInteger()) {
+                sum += ele.getInteger() * depth;
+            } else {
+                sum += helper(ele.getList(), depth + 1);
+            }
+        }
+
+        return sum;
+    }
+}
+
+
+/*
+Very Important concept! DFS by using the helper.
+Don't forget the base case
+*/
+
+
+// The BFS non-recursive version
+
+public class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        if (nestedList == null || nestedList.size() == 0) {
+            return 0;
+        }
+
         int sum = 0;
         Queue<NestedInteger> queue = new LinkedList<NestedInteger>();
         for (NestedInteger nestedInt : nestedList) {
@@ -69,6 +87,15 @@ public class Solution {
                 }
             }
         }
+
         return sum;
     }
 }
+
+/*
+The classic BFS. Think about the process.
+Use the algorithm and the data Structure!
+
+Must review
+*/
+
