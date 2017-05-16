f33e7a37e13315ee6323ec399e6da6822e8a9759
Peter <dchen@albany.edu>
2017-05-16 14:42:12 -0400

    Add 4 solutions to Valid Palindrome.java

--- practice_mac.java
+++ practice_mac.java
@@ -1,49 +1,43 @@
-// version 1
-// f[i] 表示前i个字母，最少可以被分割为多少个回文串
-// 最后return f[n] - 1
-public class Solution {
-    private boolean[][] getIsPalindrome(String s) {
-        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
-
-        for (int i = 0; i < s.length(); i++) {
-            isPalindrome[i][i] = true;
+     * @param s A string
+     * @return Whether the string is a valid palindrome
+     */
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
         }
-        for (int i = 0; i < s.length() - 1; i++) {
-            isPalindrome[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
+        if (s.length() == 0) {
+            return true;
         }
+        
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        for (int length = 2; length < s.length(); length++) {
-            for (int start = 0; start + length < s.length(); start++) {
-                isPalindrome[start][start + length]
-                    = isPalindrome[start + 1][start + length - 1] && s.charAt(start) == s.charAt(start + length);
+
+
+        // we have to split the string first and use the regex
+        String[] words = s.toLowerCase().split("[ \n\t\r.,;:!?(){]");
+        StringBuilder sb = new StringBuilder();
+        for (String word : words) {
+            sb.append(word);
+        }
+        String cleanS = sb.toString();
+        char[] charArry = cleanS.toCharArray();
+        
+        for (Character ch : charArry) {
+            stack.push(ch);
+            queue.offer(ch);
+        }
+        
+        for (int i = 0; i < charArry.length; i++) {
+            if (stack.pop() != queue.poll()) {
+                return false;
             }
         }
-
-        return isPalindrome;
-    }
-
-    public int minCut(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-
-        // preparation
-        boolean[][] isPalindrome = getIsPalindrome(s);
         
-        // initialize
-        int[] f = new int[s.length() + 1];
-        f[0] = 0;
+        return true;
         
-        // main
-        for (int i = 1; i <= s.length(); i++) {
-            f[i] = Integer.MAX_VALUE; // or f[i] = i
-            for (int j = 0; j < i; j++) {
-                if (isPalindrome[j][i - 1]) {
-                    f[i] = Math.min(f[i], f[j] + 1);
-                }
-            }
-        }
-
-        return f[s.length()] - 1;
+        
     }
+    
+    
 }
