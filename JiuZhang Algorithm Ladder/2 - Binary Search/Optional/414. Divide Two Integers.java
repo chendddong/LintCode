ea7ceb9a642211c2fca9bc8bb15453420e2fb9f6
Peter <dchen@albany.edu>
2017-05-17 10:32:53 -0400

    Add Solution to Divide Two Integers -- Bit manipulation

---
+++ JiuZhang Algorithm Ladder/2 - Binary Search/Optional/414. Divide Two Integers.java
@@ -0,0 +1,54 @@
+/**
+ *      Divide two integers without using multiplication, division and mod 
+ *      operator.If it is overflow, return 2147483647
+ * 
+ */
+
+/*
+    Example
+    Given dividend = 100 and divisor = 9, return 11.
+ */
+
+/* Use bit */
+public class Solution {
+    /**
+     * @param dividend the dividend
+     * @param divisor the divisor
+     * @return the result
+     */
+    public int divide(int dividend, int divisor) {
+        /* edge cases */
+        if (divisor == 0) {
+            return dividend >= 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE; 
+        }
+        if (dividend == 0) {
+            return 0;
+        }
+
+        boolean isNegative = (dividend < 0 && divisor > 0) || 
+                             (dividend > 0 && divisor < 0);
+
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        int result = 0;
+        while (a >= b) {
+            int shift = 0;
+            while (a >= (b << shift)) {
+                shift++;
+            }
+            a -= b << (shift - 1);
+            result += 1 << (shift - 1);
+        }
+
+        return isNegative ? -result : result;
+    }
+}
+
+/*
+    Thoughts:
+
+    1. Use left shit to compare the value with the dividend and divisor
+       find the proper value for the certain position and then add it to the
+       result.
+    2. Good example of bit manipulation
+ */
