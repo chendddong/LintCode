ad09d3af800bddbbb0c6aa0a9a134145adb5e852
Peter <dchen@albany.edu>
2017-05-19 18:37:20 -0400

    Add Solution1 First Position of Target

---
+++ JiuZhang Algorithm Ladder/2 - Binary Search/Optional/14. First Position of Target Easy.java
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        /* first comes first. */
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+        
+        return -1;
+    }
+}
