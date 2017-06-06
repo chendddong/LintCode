ffb1c21e19afdd2165de13e0e9daf8b89e335264
Peter <dchen@albany.edu>
2017-06-05 23:55:15 -0400

    Add to the outer LintCode

---
+++ 474. Lowest Common Ancestor II Easy.java
@@ -0,0 +1,66 @@
+/*
+    Given the root and two nodes in a Binary Tree. Find the lowest common ancestor
+    (LCA) of the two nodes.
+
+    The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+
+    The node has an extra attribute parent which point to the father of itself. The root's parent is null.
+ */
+
+/*
+    Example
+    For the following binary tree:
+
+      4
+     / \
+    3   7
+       / \
+      5   6
+    LCA(3, 5) = 4
+
+    LCA(5, 6) = 7
+
+    LCA(6, 7) = 7
+ */
+
+
+
+public class Solution {
+    public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root,
+                                           ParentTreeNode A,
+                                           ParentTreeNode B) {
+        ArrayList<ParentTreeNode> pathA = getPath2Root(A);
+        ArrayList<ParentTreeNode> pathB = getPath2Root(B);
+
+        int indexA = pathA.size() - 1;
+        int indexB = pathB.size() - 1;
+
+        ParentTreeNode lowestAncestor = null;
+        while (indexA >= 0 && indexB >= 0) {
+            if (pathA.get(indexA) != pathB.get(indexB)) {
+                break;
+            }
+
+            lowestAncestor = pathA.get(indexA);
+            indexA--;
+            indexB--;
+        }
+        return lowestAncestor;
+    }
+
+    private ArrayList<ParentTreeNode> getPath2Root(ParentTreeNode node) {
+        ArrayList<ParentTreeNode> path = new ArrayList<>();
+        while (node != null) {
+            path.add(node);
+            node = node.parent;
+        }
+        return path;       
+    }
+
+}
+
+/*
+    We can't use the normal sequence because we cannot make sure when they have
+    the first common element. If you really want to do it, you could try to make
+    it O(n^2) where you should loop though pathA and nested loop the pathB
+*/
---
+++ 88. Lowest Common Ancestor Medium.java
@@ -0,0 +1,139 @@
+/**
+ * Given the root and two nodes in a Binary Tree. Find the lowest
+ * common ancestor(LCA) of the two nodes.
+ */
+
+/*
+    The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+
+    Assume two nodes are exist in tree.
+
+    Example
+    For the following binary tree:
+
+      4
+     / \
+    3   7
+       / \
+      5   6
+
+    LCA(3, 5) = 4
+
+    LCA(5, 6) = 7
+
+    LCA(6, 7) = 7
+ */
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+/////////////////////
+// Solution 0 D&C  //
+/////////////////////
+
+
+public class Solution {
+    /* 
+        Thoughts:
+        Find the LCA of node A,B in the binary tree with root root
+        If we find it, then return the LCA
+        If we only find A, return A
+        If we only find B, return B
+        If we find neither of them, return null
+    */
+    /* Task: Find the LCA of node A,B in the binary tree */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B){
+        /* Base case */
+        if(root == null || root == A || root == B) {
+            return root;
+        }
+
+        /* Divide */
+        TreeNode left = lowestCommonAncestor(root.left, A, B);
+        TreeNode right = lowestCommonAncestor(root.right, A, B);
+
+        /* Solve the problem */
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        if (right != null) {
+            return right;
+        }
+        return null;
+
+    }
+}
+
+///////////////////////////
+// Solution 1 ResultType //
+///////////////////////////
+
+class ResultType {
+    public TreeNode lca;
+    public int count;
+    public ResultType(TreeNode lca, int count) {
+        this.lca = lca;
+        this.count = count;
+    }
+}
+
+public class Solution {
+    /* 
+        Thoughts:
+        Find the LCA of node A,B in the binary tree with root root
+        If we find it, then return the LCA
+        If we only find A, return A
+        If we only find B, return B
+        If we find neither of them, return null
+    */
+    /* Task: Find the LCA of node A,B in the binary tree */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B){
+        return helper(root, A, B).lca;
+    }
+
+    private ResultType helper(TreeNode root, TreeNode A, TreeNode B) {
+        if (root == null) {
+            return new ResultType(null, 0);
+        }
+        /* Base case */
+        if (root == A || root == B) {
+            return new ResultType(root, 1);
+        }
+
+        /* Divide */
+        ResultType left = helper(root.left, A, B);
+        ResultType right = helper(root.right, A, B);
+
+        /* Solve the problem */
+        if (left.count == 1 && right.count == 1) {
+            return new ResultType(root, 2);
+        }
+        if (left.count == 2) {
+            return new ResultType(left.lca, 2);
+        } 
+        if (right.count == 2) {
+            return new ResultType(right.lca, 2);
+        }
+        
+        if (left.count == 1 && right.count == 0) {
+            return new ResultType(left.lca, 1);
+        }
+        if (right.count == 1 && left.count == 0) {
+            return new ResultType(right.lca, 1);
+        }
+        
+        return new ResultType(null, 0);        
+    }
+}
