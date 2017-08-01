fba6e2a767b341b6ca1ba8595ba6278e1a7e8dbf
Peter <dchen@albany.edu>
2017-08-01 15:11:13 -0400

    Added the Optimization

--- 132. Word Search II.java
+++ 132. Word Search II.java
@@ -98,3 +98,83 @@ public class Solution {
         return result;
     }  
 }
+
+/////////////////////////
+// Backtracking + Trie //           Optimized Solution
+/////////////////////////
+
+/*
+    Intuitively, start from every cell and try to build a word in the dictionary. Backtracking (dfs) is the powerful way to exhaust every possible ways. Apparently, we need to do pruning when current character is not in any word.
+
+    How do we instantly know the current character is invalid? HashMap?
+    How do we instantly know what's the next valid character? LinkedList?
+    But the next character can be chosen from a list of characters. "Mutil-LinkedList"?
+    Combing them, Trie is the natural choice. Notice that:
+
+    TrieNode is all we need. search and startsWith are useless.
+    No need to store character at TrieNode. c.next[i] != null is enough.
+    Never use c1 + c2 + c3. Use StringBuilder.
+    No need to use O(n^2) extra space visited[m][n].
+    No need to use StringBuilder. Storing word itself at leaf node is enough.
+    No need to use HashSet to de-duplicate. Use "one time search" trie.
+
+    59ms: Use search and startsWith in Trie class like this popular solution.
+    33ms: Remove Trie class which unnecessarily starts from root in every dfs call.
+    30ms: Use w.toCharArray() instead of w.charAt(i).
+    22ms: Use StringBuilder instead of c1 + c2 + c3.
+    20ms: Remove StringBuilder completely by storing word instead of boolean in TrieNode.
+    20ms: Remove visited[m][n] completely by modifying board[i][j] = '#' directly.
+    18ms: check validity, e.g., if(i > 0) dfs(...), before going to the next dfs.
+    17ms: De-duplicate c - a with one variable i.
+    15ms: Remove HashSet completely.
+    The final run time is 15ms. Hope it helps!
+ */
+public class Solution {
+
+    public ArrayList<String> wordSearchII(char[][] board, ArrayList<String> words) {
+        ArrayList<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs (board, i, j, root, res);
+            }
+        }
+        return res;
+    }
+
+    public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+        char c = board[i][j];
+        if (c == '#' || p.next[c - 'a'] == null) return;
+        p = p.next[c - 'a'];
+        if (p.word != null) {   // found one
+            res.add(p.word);
+            p.word = null;     // de-duplicate
+        }
+
+        board[i][j] = '#';
+        if (i > 0) dfs(board, i - 1, j ,p, res); 
+        if (j > 0) dfs(board, i, j - 1, p, res);
+        if (i < board.length - 1) dfs(board, i + 1, j, p, res); 
+        if (j < board[0].length - 1) dfs(board, i, j + 1, p, res); 
+        board[i][j] = c;
+    }
+
+    public TrieNode buildTrie(ArrayList<String> words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode p = root;
+            for (char c : w.toCharArray()) {
+                int i = c - 'a';
+                if (p.next[i] == null) p.next[i] = new TrieNode();
+                p = p.next[i];
+           }
+           p.word = w;
+        }
+        return root;
+    }
+
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }     
+}
