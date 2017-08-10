249510ddd65bee095fc481d1ffb1a838252d6af7
Peter <dchen@albany.edu>
2017-08-10 08:08:46 -0400

    Change the name

---
+++ 364. Trapping Rain Water II.java
@@ -0,0 +1,22 @@
+/*
+    Given n x m non-negative integers representing an elevation map 2d where the area of each cell is 1 x 1, compute how much water it is able to trap after raining.
+
+    Example
+    Given 5*4 matrix
+
+    [12,13,0,12]
+    [13,4,13,12]
+    [13,8,10,12]
+    [12,13,12,12]
+    [13,13,13,13]
+    return 14.
+ */
+
+public class Solution {
+    /*
+        Pouring water from outside to inside.
+     */
+    public int trapRainWater(int[][] heights) {
+
+    }
+}
