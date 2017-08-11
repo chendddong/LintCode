c4f2cb8ce613930a8be211b1211dfe45d9dfd029
Peter <dchen@albany.edu>
2017-08-11 13:36:58 -0400

    Modified the solution and comment

--- 40. Implement Queue by Two Stacks Medium.java
+++ 40. Implement Queue by Two Stacks Medium.java
@@ -1,3 +1,22 @@
+/**
+ * As the title described, you should only use two stacks to implement a
+ * queue's actions.
+ */
+
+/*
+    The queue should support push(element), pop() and top() where pop is pop the first(a.k.a front) element in the queue.
+
+    Both pop and top methods should return the value of first element.
+
+    Have you met this question in a real interview? Yes
+    Example
+    push(1)
+    pop()     // return 1
+    push(2)
+    push(3)
+    top()     // return 2
+    pop()     // return 2
+ */
 
 //////////////////////
 // Shorter version  //
@@ -11,22 +30,20 @@ public class MyQueue {
         stack1 = new ArrayDeque<Integer>();
         stack2 = new ArrayDeque<Integer>();
     }
-    
+
+    /* Only push the element to 1 stack. */    
     public void push(int element) {
         stack1.push(element);
     }
 
+    /* If stack2 is empty, throw all the elements from stack1 to stack2 */
     public int pop() {
-        /* Only see if the stack we are going to pop element from empty or not
-        */ 
         if (stack2.isEmpty()) {
             this.shiftOneToTwo();
         }
         return stack2.pop();
     }
-
     public int top() {
-        /* same here */
         if (stack2.isEmpty()) {
             this.shiftOneToTwo();
         }
@@ -39,50 +56,3 @@ public class MyQueue {
         }
     }
 }
-
-////////////////////
-// longer version //
-////////////////////
-
-public class MyQueue {
-    private ArrayDeque<Integer> stack1;
-    private ArrayDeque<Integer> stack2;
-
-    public MyQueue() {
-        stack1 = new ArrayDeque<Integer>();
-        stack2 = new ArrayDeque<Integer>();
-    }
-    
-    public void push(int element) {
-        stack1.push(element);
-    }
-
-    public int pop() {
-        if (!stack2.isEmpty()) {
-            return stack2.pop();            
-        }
-        
-        if (!stack1.isEmpty()) {
-            this.shiftOneToTwo();
-        }
-        return stack2.pop();
-    }
-
-    public int top() {
-        if (!stack2.isEmpty()) {
-            return stack2.peek();            
-        }
-        
-        if (!stack1.isEmpty()) {
-            this.shiftOneToTwo();
-        }
-        return stack2.peek();
-    }
-
-    private void shiftOneToTwo() {
-        while (!stack1.isEmpty()) {
-            stack2.push(stack1.pop());
-        }
-    }
-}
-
