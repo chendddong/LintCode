ff344ffd417a71535cd88b48d58079e939092ec7
Peter <dchen@albany.edu>
2017-08-15 15:50:12 -0400

    Reviewed

--- 510. Maximal Rectangle.java
+++ 510. Maximal Rectangle.java
@@ -21,6 +21,24 @@
     We can treat this problem as the follow up of the largest rectangle in
     histogram. All we need to do is to treat the matrix like a m-row array and
     plug in with the largestRectangleArea right way.
+
+    VERY VERY VERY Important 
+
+    Algorithm:
+
+    The stack can only store value that is larger than the previous
+    value, and if it is not the case, we had dealt with it early.
+    
+    Traverse the index from 0 to length, assign cur value to the index
+    value, the last index's value is -1 or Integer.MIN_VALUE;
+    
+    if stack is not empty and cur value less than the height[peek()],
+        h = height[pop()] from the stack
+        w = i when stack is empty else w = i - peek() - 1;
+        update the max each time we are inside
+
+    Each time we push the index to the stack.
+
  */
 public class Solution {
 
@@ -28,9 +46,9 @@ public class Solution {
         int m = matrix.length;
         int n = m == 0 ? 0 : matrix[0].length;
         int[][] height = new int[m][n];
-
-        /*             Traverse the matrix and convert the matrix to height array for each
-            row 
+        /*  
+            Traverse the matrix and convert the matrix to height array for each
+            row. 
          */
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -45,52 +63,12 @@ public class Solution {
                 }
             }
         }
-
-        /* Algorithm walkthrough */
-
-        // matrix[][]
-        // [
-        //   [1, 1, 0, 0, 1],
-        //   [0, 1, 0, 0, 1],
-        //   [0, 0, 1, 1, 1],      m x n
-        //   [0, 0, 1, 1, 1],
-        //   [0, 0, 0, 0, 1]
-        // ]
-        // 
-        // 
-        // height[][]
-        // [
-        //   [1, 1, 0, 0, 1,  0],
-        //   [0, 2, 0, 0, 2,  0],
-        //   [0, 0, 1, 1, 3,  0],   m x (n + 1)
-        //   [0, 0, 2, 2, 4,  0],
-        //   [0, 0, 0, 0, 5,  0]
-        // ]        
-        // 
-        // 
-        // 
-        
         int maxArea = 0;
         for (int i = 0; i < m; i++) {
             maxArea = Math.max(largestRectangleArea(height[i]) , maxArea);
         }
         return maxArea;
     }
-
-    /* 
-        The stack can only store value that is larger than the previous
-        value, and if it is not the case, we had dealt with it early.
-        
-        Traverse the index from 0 to length, assign cur value to the index
-        value, the last index's value is -1 or Integer.MIN_VALUE;
-        
-        if stack is not empty and cur value less than the height[peek()],
-            h = height[pop()] from the stack
-            w = i when stack is empty else w = i - peek() - 1;
-            update the max each time we are inside
-
-        Each time we push the index to the stack.
-     */
     private int largestRectangleArea(int[] height) {
         if (height == null || height.length == 0) {
             return 0;
@@ -112,6 +90,28 @@ public class Solution {
     }    
 }
 
+/* Algorithm walkthrough */
+
+// matrix[][]
+// [
+//   [1, 1, 0, 0, 1],
+//   [0, 1, 0, 0, 1],
+//   [0, 0, 1, 1, 1],      m x n
+//   [0, 0, 1, 1, 1],
+//   [0, 0, 0, 0, 1]
+// ]
+// 
+// 
+// height[][]
+// [
+//   [1, 1, 0, 0, 1,  0],
+//   [0, 2, 0, 0, 2,  0],
+//   [0, 0, 1, 1, 3,  0],   m x (n + 1)
+//   [0, 0, 2, 2, 4,  0],
+//   [0, 0, 0, 0, 5,  0]
+// ]        
+
+        
 ////////////////
 // DP Version //
 ////////////////
