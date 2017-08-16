d549fe8bc5b52ad2162d0d00b83edf2503fe5fbe
Peter <dchen@albany.edu>
2017-08-15 20:25:18 -0400

    Reviewed 130

--- 130. Heapify.java
+++ 130. Heapify.java
@@ -1,20 +1,27 @@
-// Given an integer array, heapify it into a min-heap array.
+/**
+ * Given an integer array, heapify it into a min-heap array.
+ */
 
-// For a heap array A, A[0] is the root of heap, and for each A[i], A[i * 2 + 1]
-// is the left child of A[i]   and A[i * 2 + 2] is the right child of A[i].
+/*
+    For a heap array A, A[0] is the root of heap, and for each A[i], A[i * 2 +
+    1] is the left child of A[i]   and A[i * 2 + 2] is the right child of A[i].
 
-// Clarification
-// What is heap?
+    What is heap?
+    Heap is a data structure, which usually have three methods: push, pop and top. where "push" add a new element the heap, "pop" delete the minimum/maximum element in the heap, "top" return the minimum/maximum element.
 
-// Heap is a data structure, which usually have three methods: push, pop and top. where "push" add a new element the heap, "pop" delete the minimum/maximum element in the heap, "top" return the minimum/maximum element.
+    What is heapify?
+    Convert an unordered integer array into a heap array. If it is min-heap, for each element A[i], we will get A[i * 2 + 1] >= A[i] and A[i * 2 + 2] >= A[i].
 
-// What is heapify?
-// Convert an unordered integer array into a heap array. If it is min-heap, for each element A[i], we will get A[i * 2 + 1] >= A[i] and A[i * 2 + 2] >= A[i].
+    What if there is a lot of solutions?
+    Return any of them.
 
-// What if there is a lot of solutions?
-// Return any of them.
-// Example
-// Given [3,2,1,4,5], return [1,2,3,4,5] or any legal heap array.
+    Example
+    Given [3,2,1,4,5], return [1,2,3,4,5] or any legal heap array.
+ */
+
+///////////
+// Linpz //
+///////////
 
 public class Solution {
     public void heapify(int[] A) {
@@ -33,7 +40,7 @@ public class Solution {
                 break;
             }
 
-            // swap
+            /* swap */
             int temp = A[son];
             A[son] = A[k];
             A[k] = temp;
@@ -42,24 +49,34 @@ public class Solution {
     }
 }
 
-/* Much easier to understand for this version */
+////////////////////////
+// SiftDown bottom up //
+////////////////////////
+
 public class Solution {
-    /**
-     * @param A: Given an integer array
-     * @return: void
-     */
+
     private void siftdown(int[] A, int k) {
         while (k < A.length) {
             int smallest = k;
+
+            /* Has left child && left child < father */
             if (k * 2 + 1 < A.length && A[k * 2 + 1] < A[smallest]) {
                 smallest = k * 2 + 1;
             }
+
+            /* 
+                Has right child && right child < the smallest (father
+                and left child) 
+             */
             if (k * 2 + 2 < A.length && A[k * 2 + 2] < A[smallest]) {
                 smallest = k * 2 + 2;
             }
-            if (smallest == k) {
+
+            if (smallest == k) { /* No need to sift */ 
                 break;
             }
+
+            /* Swap */
             int temp = A[smallest];
             A[smallest] = A[k];
             A[k] = temp;
@@ -69,19 +86,19 @@ public class Solution {
     }
     
     public void heapify(int[] A) {
+        /* SiftDown from the bottom to top */
         for (int i = A.length / 2; i >= 0; i--) {
             siftdown(A, i);
-        } // for
+        } 
     }
 }
 
+//////////////////////////
+// Travel all the nodes //
+//////////////////////////
 
-// Version 2: This cost O(nlogn)
 public class Solution {
-    /**
-     * @param A: Given an integer array
-     * @return: void
-     */
+
     private void siftup(int[] A, int k) {
         while (k != 0) {
             int father = (k - 1) / 2;
@@ -103,7 +120,6 @@ public class Solution {
     }
 }
 
-
 /*
-    We have to draw the whole thing while writing the code.
+    Draw the HEAP when writing the code.
 */
