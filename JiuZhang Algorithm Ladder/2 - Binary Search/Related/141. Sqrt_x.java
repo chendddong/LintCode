5b19f900d45aa498852791c55394cab74f4f2b93
Peter <dchen@albany.edu>
2017-08-16 16:43:53 -0400

    Updated 141

--- JiuZhang Algorithm Ladder/2 - Binary Search/Related/141. Sqrt_x.java
+++ JiuZhang Algorithm Ladder/2 - Binary Search/Related/141. Sqrt_x.java
@@ -15,15 +15,11 @@
     sqrt(10) = 3
  */
 
-///////////////////////////////////////////////////
-// Solution 1 StraightForward Using Math.sqrt(); //
-///////////////////////////////////////////////////
+////////////////////////////////////////
+// StraightForward Using Math.sqrt(); //
+////////////////////////////////////////
 
 class Solution {
-    /**
-     * @param x: An integer
-     * @return: The sqrt of x
-     */
     public int sqrt(int x) {
         if (x <= 0) {
             return 0;
@@ -32,22 +28,19 @@ class Solution {
     }
 }
 
-////////////////////////////////////
-// Solution 2 Using Binary Search //
-////////////////////////////////////
 
+/////////////////////////
+// Using Binary Search //
+/////////////////////////
 
 class Solution {
-    /**
-     * @param x: An integer
-     * @return: The sqrt of x
-     */
+
     public static int sqrt(int x) {
         if (x <= 0) {
             return 0;
         }
 
-        /* Find the min index and the max index*/
+        /* Narrow down the search */
         long tempSqrt = (long) x;
         while (tempSqrt * tempSqrt > x) {
             tempSqrt /= 2;
@@ -58,11 +51,7 @@ class Solution {
             return (int) tempSqrt;
         }
 
-        /* 999999999 */
-        /* 30517 ~ 61035 */
-        long start = tempSqrt;  
-        long end = tempSqrt * 2;
-
+        long start = tempSqrt, end = start * 2;
 
         while (start + 1 < end) {
             long mid = start + (end - start) / 2;
@@ -87,20 +76,16 @@ class Solution {
     }
 }
 
-/////////////////////////////////////////////
-// Solution 3 Using Binary Search Directly //
-/////////////////////////////////////////////
+//////////////////////////////////
+// Using Binary Search Directly // 
+//////////////////////////////////
 
-/* find the last number which square of it <= x */
+/* Treat this as the template of value binary search */
 
 class Solution {
-    /**
-     * @param x: An integer
-     * @return: The sqrt of x
-     */
+
     public int sqrt(int x) {
-        // find the last number which square of it <= x
-        long start = 1, end = x;
+        long start = 1, end = x; /* Should use long instead of int */
         while (start + 1 < end) {
             long mid = start + (end - start) / 2;
             if (mid * mid <= x) {
@@ -111,7 +96,7 @@ class Solution {
         }
         
         if (end * end <= x) {
-            return (int) end;
+            return (int) end; /* Cast to return type */
         }
         return (int) start;
     }
