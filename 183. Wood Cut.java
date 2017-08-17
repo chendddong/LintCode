1a2d574df298fa9232af0f333f7a67e2fa53d68c
Peter <dchen@albany.edu>
2017-08-16 23:18:50 -0400

    Updated the solution

--- 183. Wood Cut.java
+++ 183. Wood Cut.java
@@ -12,49 +12,47 @@
     end is 228
  */
 
-public class Solution{
+public class Solution {
+
     public int woodCut(int[] L, int k) {
-        /* find the max piece */
-        int max = 0;
-        for (int i = 0; i < L; ++i) {
-            max = Math.max(max, L[i]);
-        }
 
-        /* find the largest length that can cut the number of pieces needed */
+        /* Narrow down the binary search interval */
         int start = 1;
-        int end = max;
-
+        int end = findMax(L);
+        
+        /* Template */
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            if (count(L, mid) >= k) {
+            if (count(L, mid) >= k) { /* Check */
                 start = mid;
             } else {
                 end = mid;
             }
         }
-
-        /* find the largest one so starts with the lareger one */
+        
+        /* Need the largest one so we starts with the larger one */ 
         if (count(L, end) >= k) {
             return end;
         }
-
+        
         if (count(L, start) >= k) {
             return start;
         }
-
+        
         return 0;
     }
-
-    private int count(int[] L, int length) {
-        int sum = 0;
-        for (int i = 0; i < L.length; ++i) {
-            sum += L[i] / length;
+    private int findMax(int[] L) {
+        int max = 0;
+        for (int wood : L) {
+            max = Math.max(wood, max);
         }
-
-        return sum;
+        return max;
     }
-}
-
-/*
-    The process and how to translate the thing into thought process
- */
+    private int count(int[] L, int pc) {
+        int count = 0;
+        for (int wood : L) {
+            count += wood / pc;
+        }
+        return count;
+    }
+};
