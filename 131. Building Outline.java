e27ac59af533464a6687eff4fa0fab2b24a2872c
Peter <dchen@albany.edu>
2017-08-17 23:03:46 -0400

    Updated the heap version

--- 131. Building Outline.java
+++ 131. Building Outline.java
@@ -1,6 +1,169 @@
-/* Comeback later for the scanning line lecture */
+/*
+    Given N buildings in a x-axis，each building is a rectangle and can be
+    represented by a triple (start, end, height)，where start is the start position on x-axis, end is the end position on x-axis and height is the height of the building. Buildings may overlap if you see them from far away，find the outline of them。
 
-import java.util.*;
+    An outline can be represented by a triple, (start, end, height), where start is the start position on x-axis of the outline, end is the end position on x-axis and height is the height of the outline.
+ */
+
+/*
+    Please merge the adjacent outlines if they have the same height and make sure different outlines cant overlap on x-axis.
+
+    Have you met this question in a real interview? Yes
+    Example
+    Given 3 buildings：
+
+    [
+      [1, 3, 3],
+      [2, 4, 4],
+      [5, 6, 1]
+    ]
+    The outlines are：
+
+    [
+      [1, 2, 3],
+      [2, 4, 4],
+      [5, 6, 1]
+    ]
+ */
+
+/*
+    Given N buildings in a x-axis，each building is a rectangle and can be
+    represented by a triple (start, end, height)，where start is the start position on x-axis, end is the end position on x-axis and height is the height of the building. Buildings may overlap if you see them from far away，find the outline of them。
+
+    An outline can be represented by a triple, (start, end, height), where start is the start position on x-axis of the outline, end is the end position on x-axis and height is the height of the outline.
+ */
+
+/*
+    Please merge the adjacent outlines if they have the same height and make sure different outlines cant overlap on x-axis.
+
+    Have you met this question in a real interview? Yes
+    Example
+    Given 3 buildings：
+
+    [
+      [1, 3, 3],
+      [2, 4, 4],
+      [5, 6, 1]
+    ]
+    The outlines are：
+
+    [
+      [1, 2, 3],
+      [2, 4, 4],
+      [5, 6, 1]
+    ]
+ */
+
+
+//////////
+// Heap //      Use PriorityQueue as a heap O(n log n)
+//////////
+
+/* Go through the algorithm later */
+public class Solution {
+
+    class Edge {
+        int pos;
+        int height;
+        boolean isStart;
+
+        public Edge(int pos, int height, boolean isStart) {
+            this.pos = pos;
+            this.height = height;
+            this.isStart = isStart;
+        }
+    }
+    /* How to sort the Edge ?!*/
+    class EdgeComparator implements Comparator<Edge> {
+        @Override
+        public int compare(Edge arg1, Edge arg2) {
+            Edge l1 = (Edge) arg1;
+            Edge l2 = (Edge) arg2;
+            if (l1.pos != l2.pos)
+                return compareInteger(l1.pos, l2.pos);
+            if (l1.isStart && l2.isStart) {
+                return compareInteger(l2.height, l1.height);
+            }
+            if (!l1.isStart && !l2.isStart) {
+                return compareInteger(l1.height, l2.height);
+            }
+            return l1.isStart ? -1 : 1;
+        }
+
+        int compareInteger(int a, int b) {
+            return a <= b ? -1 : 1;
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> buildingOutline(int[][] buildings) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+
+        if (buildings == null || buildings.length == 0
+            || buildings[0].length == 0) {
+            return res;
+        }
+        ArrayList<Edge> edges = new ArrayList<Edge>();
+        for (int[] building : buildings) {
+            Edge startEdge = new Edge(building[0], building[2], true);
+            edges.add(startEdge);
+            Edge endEdge = new Edge(building[1], building[2], false);
+            edges.add(endEdge);
+        }
+        Collections.sort(edges, new EdgeComparator());
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(1, Collections.reverseOrder());
+        ArrayList<Integer> now = null;
+        
+        for (Edge edge : edges) {
+            if (edge.isStart) {s
+                if (heap.isEmpty() || edge.height > heap.peek()) {
+                    now = new ArrayList<Integer>(Arrays.asList(edge.pos,
+                        edge.height));
+                    res.add(now);
+                }
+                heap.add(edge.height);
+            } else {
+                heap.remove(edge.height);
+                if (heap.isEmpty() || edge.height > heap.peek()) {
+                    if (heap.isEmpty()) {
+                        now = new ArrayList<Integer>(Arrays.asList(edge.pos, 0));
+                    } else {
+                        now = new ArrayList<Integer>(Arrays.asList(edge.pos,
+                            heap.peek()));
+                    }
+                    res.add(now);
+                }
+            }
+        }
+        return output(res);
+    }
+
+    ArrayList<ArrayList<Integer>> output(ArrayList<ArrayList<Integer>> res) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        if (res.size() > 0) {
+            int pre = res.get(0).get(0);
+            int height = res.get(0).get(1);
+            for (int i = 1; i < res.size(); i++) {
+                ArrayList<Integer> now = new ArrayList<Integer>();
+                int id = res.get(i).get(0);
+                if (height > 0) {
+                    now.add(pre);
+                    now.add(id);
+                    now.add(height);
+                    ans.add(now);
+                }
+                pre = id;
+                height = res.get(i).get(1);
+            }
+        }
+        return ans;
+    }  
+
+}
+
+//////////////
+// HashHeap //                  O(n)
+//////////////
 
 public class Solution {
 
@@ -204,27 +367,6 @@ public class Solution {
         }
     }
 
-    ArrayList<ArrayList<Integer>> output(ArrayList<ArrayList<Integer>> res) {
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-        if (res.size() > 0) {
-            int pre = res.get(0).get(0);
-            int height = res.get(0).get(1);
-            for (int i = 1; i < res.size(); i++) {
-                ArrayList<Integer> now = new ArrayList<Integer>();
-                int id = res.get(i).get(0);
-                if (height > 0) {
-                    now.add(pre);
-                    now.add(id);
-                    now.add(height);
-                    ans.add(now);
-                }
-                pre = id;
-                height = res.get(i).get(1);
-            }
-        }
-        return ans;
-    }
-
     public ArrayList<ArrayList<Integer>> buildingOutline(int[][] buildings) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
 
@@ -267,5 +409,26 @@ public class Solution {
         }
         return output(res);
     }
+
+    ArrayList<ArrayList<Integer>> output(ArrayList<ArrayList<Integer>> res) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        if (res.size() > 0) {
+            int pre = res.get(0).get(0);
+            int height = res.get(0).get(1);
+            for (int i = 1; i < res.size(); i++) {
+                ArrayList<Integer> now = new ArrayList<Integer>();
+                int id = res.get(i).get(0);
+                if (height > 0) {
+                    now.add(pre);
+                    now.add(id);
+                    now.add(height);
+                    ans.add(now);
+                }
+                pre = id;
+                height = res.get(i).get(1);
+            }
+        }
+        return ans;
+    }    
 
 }
