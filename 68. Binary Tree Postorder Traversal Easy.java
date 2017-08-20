f24c55234ab0bb075a117454bc587d62e2a4cd67
Peter <dchen@albany.edu>
2017-08-20 01:57:00 -0400

    Added unified solution

--- 68. Binary Tree Postorder Traversal Easy.java
+++ 68. Binary Tree Postorder Traversal Easy.java
@@ -166,3 +166,64 @@ public class Solution {
        ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
  */
+
+/////////////////
+// Interesting //
+/////////////////
+
+/*
+    Summarize the iterative implementation for preorder, inorder, and postorder
+    traverse.
+
+    Pre Order Traverse
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while(!stack.isEmpty() || p != null) {
+            if(p != null) {
+                stack.push(p);
+                result.add(p.val);  // Add before going to children
+                p = p.left;
+            } else {
+                TreeNode node = stack.pop();
+                p = node.right;   
+            }
+        }
+        return result;
+    }
+    In Order Traverse
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while(!stack.isEmpty() || p != null) {
+            if(p != null) {
+                stack.push(p);
+                p = p.left;
+            } else {
+                TreeNode node = stack.pop();
+                result.add(node.val);  // Add after all left children
+                p = node.right;   
+            }
+        }
+        return result;
+    }
+    Post Order Traverse
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while(!stack.isEmpty() || p != null) {
+            if(p != null) {
+                stack.push(p);
+                result.addFirst(p.val);  // Reverse the process of preorder
+                p = p.right;             // Reverse the process of preorder
+            } else {
+                TreeNode node = stack.pop();
+                p = node.left;           // Reverse the process of preorder
+            }
+        }
+        return result;
+    }
+ */     
