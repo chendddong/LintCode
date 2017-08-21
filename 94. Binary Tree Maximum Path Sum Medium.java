e61e1a1988c3559f1f015f4afe636ed2b08cb4e2
Peter <dchen@albany.edu>
2017-08-21 16:38:59 -0400

    Added 94

--- 94. Binary Tree Maximum Path Sum Medium.java
+++ 94. Binary Tree Maximum Path Sum Medium.java
@@ -1,4 +1,4 @@
-*
+/*
  * Given a binary tree, find the maximum path sum. The path may start and
  * end at any node in the tree.
  */
@@ -26,9 +26,9 @@
  * }
  */
 
-///////////////////////////////////////////////////
-// Solution 0 SinglePath must contain >= 1 nodes //
-///////////////////////////////////////////////////
+////////////////////////////////////////
+// SinglePath must contain >= 1 nodes //
+////////////////////////////////////////
 
 public class Solution {
     private class ResultType {
@@ -68,9 +68,9 @@ public class Solution {
     }
 }
 
-////////////////////////////////////////
-// Solution 1 singlePath can be empty //
-////////////////////////////////////////
+/////////////////////////////
+// singlePath can be empty //
+/////////////////////////////
 
 public class Solution {
     private class ResultType {
@@ -85,11 +85,12 @@ public class Solution {
         if (root == null) {
             return new ResultType(0, Integer.MIN_VALUE);
         }
-        // Divide
+
+        /* Divide */
         ResultType left = helper(root.left);
         ResultType right = helper(root.right);
 
-        // Conquer
+        /* Conquer */
         int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
         singlePath = Math.max(singlePath, 0);
 
@@ -115,5 +116,37 @@ public class Solution {
     draw it accordingly it's a must
  */
 
+
+/*
+    Thought:
+
+    A path from start to end, goes up on the tree for 0 or more steps, then goes down for 0 or more steps. Once it goes down, it can't go up. Each path has a highest node, which is also the lowest common ancestor of all other nodes on the path.
+
+    A recursive method maxPathDown(TreeNode node):
+        (1) computes the maximum path sum with highest node is the input node, update maximum if necessary 
+        (2) returns the maximum sum of the path that can be extended to input node's parent.    
+ */
+
+///////////////////////
+// A Global variable //         Recursive is much cleaner
+///////////////////////
+
+public class Solution {
+    int maxValue;
     
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+    
+    private int maxPathDown(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left)); /* Pruning negative */
+        int right = Math.max(0, maxPathDown(node.right));
+        
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}   
  
