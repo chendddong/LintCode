a0088afb9bbc40d27ff882ff5e5a34d961488c33
Peter <dchen@albany.edu>
2017-08-24 15:10:54 -0400

    Added 603

---
+++ temp.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int[] f = new int[nums.length];
+        int[] pre = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            f[i] = 1;
+            pre[i] = i;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && f[i] < f[j] + 1) {
+                    f[i] = f[j] + 1;
+                    pre[i] = j;
+                }
+            }
+        }
+        
+        List<Integer> ans = new ArrayList<Integer>();
+        if (nums.length == 0) {
+            return ans;
+        }
+        int max = 0;
+        int max_i = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (f[i] > max) {
+                max = f[i];
+                max_i = i;
+            }
+        }
+        ans.add(nums[max_i]);
+        while (max_i != pre[max_i]) {
+            max_i = pre[max_i];
+            ans.add(nums[max_i]);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
