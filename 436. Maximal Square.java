8ec36f6630a21670cd44232449b0130f528ca07a
Peter <dchen@albany.edu>
2017-09-01 10:45:00 -0400

    Added 2 more solutions

--- 436. Maximal Square.java
+++ 436. Maximal Square.java
@@ -14,6 +14,10 @@
     Return 4.
  */
 
+/////////////////
+// DP template //
+/////////////////
+
 public class Solution {
     /*
         Type 1 With axis.
@@ -27,18 +31,17 @@ public class Solution {
             n = matrix[0].length;
         else
             return ans;
-        
-        
         /* 
             State : the length of the square using bottom right corner to
             extend the square / Init
          */
-        int dp[][] = new int[m][n];
+        // int dp[][] = new int[m][n];            
+        int dp[][] = new int[2][n];
         for (int i = 0; i < m; i++) {
-            dp[i][0] = matrix[i][0];
+            dp[i%2][0] = matrix[i][0];
         }
-        for (int i = 0; i < n; i++) {
-            dp[0][i] = matrix[0][i];
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j];
         }        
 
         /* Function */
@@ -47,12 +50,12 @@ public class Solution {
             ans = Math.max(dp[i][0], ans); 
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 1)
-                    dp[i][j] = Math.min(dp[i-1][j-1], 
-                               Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                    dp[i%2][j] = Math.min(dp[(i-1)%2][j-1], 
+                               Math.min(dp[(i-1)%2][j], dp[i%2][j-1])) + 1;
                 else 
-                    dp[i][j] = 0;
+                    dp[i%2][j] = 0;
                 
-                ans = Math.max(dp[i][j], ans); /* Update */    
+                ans = Math.max(dp[i%2][j], ans); /* Update */    
             }
         }
 
@@ -60,3 +63,73 @@ public class Solution {
         return ans * ans;
     }
 }
+
+/////////////
+// Axis DP //
+/////////////
+
+class Solution {
+    public int maxSquare(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = rows > 0 ? matrix[0].length : 0;
+        /* The length of the square length we can get from this point */
+        int[][] dp = new int[rows][cols];
+        int maxsqlen = 0;
+
+        for (int i = 0 ; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] == 1 ? 1 : 0; /* Init */
+                    maxsqlen = Math.max(maxsqlen, dp[i][j]); /* Update */
+                } else if (matrix[i][j] == 1) {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[i][j]); /* Update again */
+                }
+            }
+        }
+        return maxsqlen * maxsqlen;
+    }
+}
+
+////////////////////////
+// Axis + sequence DP //
+////////////////////////
+
+public class Solution {
+    public int maxSquare(int[][] a) {
+        /* 
+            An extra row & col would make the initialization much more easier 
+
+            matrix a:
+
+            1 0 1 0 0
+            1 0 1 1 1
+            1 1 1 1 1
+            1 0 0 1 0   
+
+            dp matrix:
+              0 0 0 0 0 0    
+              0 1 0 1 0 0
+              0 1 0 1 1 1
+              0 1 1 1 1 1
+              0 1 0 0 1 0                        
+        */
+        if(a.length == 0) return 0;
+        int m = a.length, n = a[0].length, result = 0;
+
+        int[][] b = new int[m+1][n+1];
+
+        for (int i = 1 ; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(a[i-1][j-1] == 1) { /* Where we need to take actions */
+                    b[i][j] = Math.min(Math.min(b[i][j-1] , b[i-1][j-1]), b[i-1][j])
+                    + 1; 
+                    result = Math.max(b[i][j], result); /* update result */
+                }
+            }
+        }
+
+        return result * result;
+    }
+}
+
