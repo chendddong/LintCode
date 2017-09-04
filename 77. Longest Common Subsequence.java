20ff6dfff6f8e2430a7dd4a59a75c036357463ce
Peter <dchen@albany.edu>
2017-09-04 16:56:45 -0400

    Added 77

--- 77. Longest Common Subsequence.java
+++ 77. Longest Common Subsequence.java
@@ -37,16 +37,13 @@ public class Solution {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 /* Should be i - 1 and j - 1*/                
-                if (A.charAt(i - 1) == B.charAt(j - 1)) { 
+                if (A.charAt(i - 1) == B.charAt(j - 1)) 
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
+                else 
                     dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-                    
-                }
             }
         }
         
         return dp[m][n];
-        
     }
 }
