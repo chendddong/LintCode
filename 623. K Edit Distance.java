679766a44a1c17955fecc6cabe6fbfc115ac2f63
Peter <dchen@albany.edu>
2017-09-04 16:48:19 -0400

    Reviewed the algorithm

--- 623. K Edit Distance.java
+++ 623. K Edit Distance.java
@@ -19,16 +19,16 @@
 /* All the possible k are k, k - 1, ..., 0; */
 
 ///////////////
-// Trie + DP //         Comeback later
+// Trie + DP //         Walkthrough later
 ///////////////
 
 class TrieNode {
-    // Initialize your data structure here.
+    /* Attributes in Trie */
     public TrieNode[] children;
     public boolean hasWord;
     public String str;
     
-    // Initialize your data structure here.
+    /* Initialize the children and the hasWord */
     public TrieNode() {
         children = new TrieNode[26];
         for (int i = 0; i < 26; ++i)
@@ -36,31 +36,39 @@ class TrieNode {
         hasWord = false;
     }
 
-    // Adds a word into the data structure.
+    /* Adds a word into the data structure. */
     static public void addWord(TrieNode root, String word) {
-        TrieNode now = root;
-        for(int i = 0; i < word.length(); i++) {
+        TrieNode now = root; /* Traverse pointer */
+
+        for(int i = 0; i < word.length(); i++) { /* traverse every char */
             Character c = word.charAt(i);
             if (now.children[c - 'a'] == null) {
-                now.children[c - 'a'] = new TrieNode();
+                now.children[c - 'a'] = new TrieNode(); /* Create new child */
             }
-            now = now.children[c - 'a'];
+            now = now.children[c - 'a']; /* Or get the child */
         }
-        now.str = word;
-        now.hasWord = true;
+        now.str = word; /* The whole word */
+        now.hasWord = true; /* Mark the word */
     }
 }
 
 public class Solution {
 
     public List<String> kDistance(String[] words, String target, int k) {
-        TrieNode root = new TrieNode();
+
+        TrieNode root = new TrieNode(); /* Need a virtual root */
         for (int i = 0; i < words.length; i++)
-            TrieNode.addWord(root, words[i]);
+            TrieNode.addWord(root, words[i]); /* Add words to the Trie */
 
         List<String> result = new ArrayList<String>();
 
         int n = target.length();
+
+        /* 
+            State: dp[i] means walking down the trie from the virtual root to
+            the current node, the min edit distance between the formed prefix
+            and the target's 0 to ith characters.
+         */
         int[] dp = new int[n + 1];
         for (int i = 0; i <= n; ++i)
             dp[i] = i;
@@ -70,25 +78,28 @@ public class Solution {
     }
 
     public void find(TrieNode node, List<String> result, int k, String target, int[] dp) {
+
         int n = target.length();
 
-        // dp[i] 表示从Trie的root节点走到当前node节点，形成的Prefix
-        // 和 target的前i个字符的最小编辑距离
-        if (node.hasWord && dp[n] <= k) {
+        if (node.hasWord && dp[n] <= k) { /* Where the answer satisfies */
             result.add(node.str);
         }
-        int[] next = new int[n + 1];
+
+        /* Each search we need to create a new dp */
+        int[] next = new int[n + 1]; 
         for (int i = 0; i <= n; ++i)
             next[i] = 0;
 
-        for (int i = 0; i < 26; ++i)
+        for (int i = 0; i < 26; ++i) /* 26 Characters */
             if (node.children[i] != null) {
-                next[0] = dp[0] + 1;
+                next[0] = dp[0] + 1; /* Init */
                 for (int j = 1; j <= n; j++) {
-                    if (target.charAt(j - 1) - 'a' == i) {
-                        next[j] = Math.min(dp[j - 1], Math.min(next[j - 1] + 1, dp[j] + 1));
-                    } else {
-                        next[j] = Math.min(dp[j - 1] + 1, Math.min(next[j - 1] + 1, dp[j] + 1));
+                    if (target.charAt(j - 1) - 'a' == i) { /* Matches */
+                        next[j] = Math.min(dp[j - 1], Math.min(next[j - 1] + 1,
+                            dp[j] + 1)); /* Check two j - 1 check one dp j */
+                    } else { /* Does not match */
+                        next[j] = Math.min(dp[j - 1] + 1, Math.min(next[j - 1] + 1, dp[j] + 1)); 
+                        /* Check two j - 1 check one dp j */
                     }
                 }
                 find(node.children[i], result, k, target, next);
